1fce6470d386d3580d00413835a573b00d731ca6
Steven Giesel <stgiesel35@gmail.com>
2021-06-27 12:58:39 +0200

    Initial commit

---
+++ LinkDotNet.Blog.Web/AppConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace LinkDotNet.Blog.Web
+{
+    public record AppConfiguration
+    {
+        public string LinkedinAccountUrl { get; init; }
+        public bool HasLinkedinAccount => !string.IsNullOrEmpty(LinkedinAccountUrl);
+        
+        public string GithubAccountUrl { get; init; }
+        public bool HasGithubAccount => !string.IsNullOrEmpty(LinkedinAccountUrl);
+    }
+}
---
+++ LinkDotNet.Blog.Web/AppConfigurationFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace LinkDotNet.Blog.Web
+{
+    public static class AppConfigurationFactory
+    {
+        public static AppConfiguration Create(IConfiguration config)
+        {
+            var configuration = new AppConfiguration
+            {
+                GithubAccountUrl = config["GithubAccountUrl"],
+                LinkedinAccountUrl = config["LinkedInAccountUrl"]
+            };
+            return configuration;
+        }
+    }
+}
