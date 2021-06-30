bcff855c36545b2ab5cfd19a680eb3c481d3f994
Steven Giesel <stgiesel35@gmail.com>
2021-06-30 09:15:56 +0200

    Fixed formatting issues

--- LinkDotNet.Blog.Web/Authentication/Auth0/Auth0Extensions.cs
+++ LinkDotNet.Blog.Web/Authentication/Auth0/Auth0Extensions.cs
@@ -9,73 +9,73 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace LinkDotNet.Blog.Web.Authentication.Auth0
 {
-  public static class Auth0Extensions
-  {
-    public static void UseAuth0Authentication(this IServiceCollection services, IConfiguration configuration)
+    public static class Auth0Extensions
     {
-      services.Configure<CookiePolicyOptions>(options =>
-      {
-        options.CheckConsentNeeded = _ => true;
-        options.MinimumSameSitePolicy = SameSiteMode.None;
-      });
-
-      services.AddAuthentication(options =>
+        public static void UseAuth0Authentication(this IServiceCollection services, IConfiguration configuration)
         {
-          options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-          options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-          options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        })
-        .AddCookie()
-        .AddOpenIdConnect("Auth0", options =>
-        {
-          // Set the authority to your Auth0 domain
-          var auth0 = configuration.GetSection("Auth0").Get<Auth0Information>();
-          options.Authority = $"https://{auth0.Domain}";
-          options.ClientId = auth0.ClientId;
-          options.ClientSecret = auth0.ClientSecret;
-
-          options.ResponseType = "code";
-
-          options.Scope.Clear();
-          options.Scope.Add("openid");
-
-          // Set the callback path, so Auth0 will call back to http://localhost:1234/callback
-          // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
-          options.CallbackPath = new PathString("/callback");
-
-          // Configure the Claims Issuer to be Auth0
-          options.ClaimsIssuer = "Auth0";
-
-          options.Events = new OpenIdConnectEvents
-          {
-            // handle the logout redirection
-            OnRedirectToIdentityProviderForSignOut = context =>
+            services.Configure<CookiePolicyOptions>(options =>
             {
-              var logoutUri = $"https://{auth0.Domain}/v2/logout?client_id={auth0.ClientId}";
+                options.CheckConsentNeeded = _ => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
 
-              var postLogoutUri = context.Properties.RedirectUri;
-              if (!string.IsNullOrEmpty(postLogoutUri))
+            services.AddAuthentication(options =>
               {
-                if (postLogoutUri.StartsWith("/"))
-                {
-                  // transform to absolute
-                  var request = context.Request;
-                  postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
-                }
+                  options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                  options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                  options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+              })
+              .AddCookie()
+              .AddOpenIdConnect("Auth0", options =>
+              {
+                  // Set the authority to your Auth0 domain
+                  var auth0 = configuration.GetSection("Auth0").Get<Auth0Information>();
+                  options.Authority = $"https://{auth0.Domain}";
+                  options.ClientId = auth0.ClientId;
+                  options.ClientSecret = auth0.ClientSecret;
 
-                logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
-              }
+                  options.ResponseType = "code";
 
-              context.Response.Redirect(logoutUri);
-              context.HandleResponse();
+                  options.Scope.Clear();
+                  options.Scope.Add("openid");
 
-              return Task.CompletedTask;
-            },
-          };
-        });
+                  // Set the callback path, so Auth0 will call back to http://localhost:1234/callback
+                  // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+                  options.CallbackPath = new PathString("/callback");
 
-      services.AddHttpContextAccessor();
-      services.AddScoped<ILoginManager, Auth0LoginManager>();
+                  // Configure the Claims Issuer to be Auth0
+                  options.ClaimsIssuer = "Auth0";
+
+                  options.Events = new OpenIdConnectEvents
+                  {
+                      // handle the logout redirection
+                      OnRedirectToIdentityProviderForSignOut = context =>
+                      {
+                          var logoutUri = $"https://{auth0.Domain}/v2/logout?client_id={auth0.ClientId}";
+
+                          var postLogoutUri = context.Properties.RedirectUri;
+                          if (!string.IsNullOrEmpty(postLogoutUri))
+                          {
+                              if (postLogoutUri.StartsWith("/"))
+                              {
+                                  // transform to absolute
+                                  var request = context.Request;
+                                  postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                              }
+
+                              logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+                          }
+
+                          context.Response.Redirect(logoutUri);
+                          context.HandleResponse();
+
+                          return Task.CompletedTask;
+                      },
+                  };
+              });
+
+            services.AddHttpContextAccessor();
+            services.AddScoped<ILoginManager, Auth0LoginManager>();
+        }
     }
-  }
 }
