156c6377d00559663998953184f9e1518cbb2765
Steven Giesel <stgiesel35@gmail.com>
2021-06-30 18:30:23 +0200

    Add meta tags for SEO

--- LinkDotNet.Blog.Web/Shared/MarkdownComponentBase.cs
+++ LinkDotNet.Blog.Web/Shared/MarkdownComponentBase.cs
@@ -5,6 +5,8 @@ namespace LinkDotNet.Blog.Web.Shared
 {
     public class MarkdownComponentBase : ComponentBase
     {
+        private static readonly MarkdownPipeline MarkdownPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().UseEmojiAndSmiley().Build();
+
         protected static MarkupString RenderMarkupString(string content)
         {
             if (string.IsNullOrEmpty(content))
@@ -12,9 +14,7 @@ namespace LinkDotNet.Blog.Web.Shared
                 return default;
             }
 
-            var markdownPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().UseEmojiAndSmiley().Build();
-
-            return (MarkupString)Markdown.ToHtml(content, markdownPipeline);
+            return (MarkupString)Markdown.ToHtml(content, MarkdownPipeline);
         }
     }
 }
