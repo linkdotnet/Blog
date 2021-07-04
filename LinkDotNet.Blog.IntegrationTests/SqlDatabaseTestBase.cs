2407ffd29fb9bfe9df0d4d570ee347ff63ac07b8
Steven Giesel <stgiesel35@gmail.com>
2021-07-04 16:28:28 +0200

    Use InMemory Database for integration tests

--- LinkDotNet.Blog.IntegrationTests/SqlDatabaseTestBase.cs
+++ LinkDotNet.Blog.IntegrationTests/SqlDatabaseTestBase.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Data.Common;
 using System.Threading.Tasks;
 using LinkDotNet.Infrastructure.Persistence.Sql;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 
@@ -11,7 +13,7 @@ namespace LinkDotNet.Blog.IntegrationTests
         protected SqlDatabaseTestBase()
         {
             var options = new DbContextOptionsBuilder()
-                .UseSqlite("Data Source=IntegrationTest.db")
+                .UseSqlite(CreateInMemoryConnection())
                 .Options;
             DbContext = new BlogPostContext(options);
             BlogPostRepository = new BlogPostRepository(new BlogPostContext(options));
@@ -35,6 +37,15 @@ namespace LinkDotNet.Blog.IntegrationTests
         {
             await DbContext.Database.EnsureDeletedAsync();
             await DbContext.DisposeAsync();
+        }
+
+        private static DbConnection CreateInMemoryConnection()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+
+            connection.Open();
+
+            return connection;
         }
     }
 }
