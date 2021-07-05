70cbb67529977c21b1fb972c69c8481dcc92714e
Steven Giesel <stgiesel35@gmail.com>
2021-07-05 14:23:51 +0200

    Use Bootstrap

--- LinkDotNet.Blog.Web/Shared/MarkdownComponentBase.cs
+++ LinkDotNet.Blog.Web/Shared/MarkdownComponentBase.cs
@@ -5,7 +5,12 @@ namespace LinkDotNet.Blog.Web.Shared
 {
     public class MarkdownComponentBase : ComponentBase
     {
-        private static readonly MarkdownPipeline MarkdownPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().UseEmojiAndSmiley().UseCitations().Build();
+        private static readonly MarkdownPipeline MarkdownPipeline = new MarkdownPipelineBuilder()
+            .UseAdvancedExtensions()
+            .UseEmojiAndSmiley()
+            .UseCitations()
+            .UseBootstrap()
+            .Build();
 
         protected static MarkupString RenderMarkupString(string content)
         {
