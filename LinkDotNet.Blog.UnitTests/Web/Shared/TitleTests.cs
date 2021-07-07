02db1ab19ba2807eeff87075f26557a5ec9b35bb
Steven Giesel <stgiesel35@gmail.com>
2021-07-07 08:41:55 +0200

    Added TitleTests

---
+++ LinkDotNet.Blog.UnitTests/Web/Shared/TitleTests.cs
@@ -0,0 +1,21 @@
+﻿using Bunit;
+using FluentAssertions;
+using LinkDotNet.Blog.Web.Shared;
+using Xunit;
+
+namespace LinkDotNet.Blog.UnitTests.Web.Shared
+{
+    public class TitleTests
+    {
+        [Fact]
+        public void ShouldSetTitle()
+        {
+            using var ctx = new TestContext();
+            ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+
+            ctx.RenderComponent<Title>(c => c.Add(p => p.Value, "New Title"));
+
+            ctx.JSInterop.VerifyInvoke("setDocumentTitle").Arguments[0].Should().Be("New Title");
+        }
+    }
+}
