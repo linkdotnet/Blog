3ab739d2eef8512e08640a1169b32b100d7aad3a
Steven Giesel <stgiesel35@gmail.com>
2021-07-07 12:46:56 +0200

    Added Tests

---
+++ LinkDotNet.Blog.IntegrationTests/Web/Shared/Admin/BlogPostAdminActionsTests.cs
@@ -0,0 +1,67 @@
+﻿using Blazored.Toast.Services;
+using Bunit;
+using Bunit.TestDoubles;
+using FluentAssertions;
+using LinkDotNet.Blog.Web.Shared.Admin;
+using LinkDotNet.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace LinkDotNet.Blog.IntegrationTests.Web.Shared.Admin
+{
+    public class BlogPostAdminActionsTests
+    {
+        [Fact]
+        public void ShouldDeleteBlogPostWhenOkClicked()
+        {
+            const string blogPostId = "2";
+            var repositoryMock = new Mock<IRepository>();
+            using var ctx = new TestContext();
+            ctx.AddTestAuthorization().SetAuthorized("s");
+            ctx.Services.AddSingleton(repositoryMock.Object);
+            ctx.Services.AddSingleton(new Mock<IToastService>().Object);
+            var cut = ctx.RenderComponent<BlogPostAdminActions>(s => s.Add(p => p.BlogPostId, blogPostId));
+            cut.Find("#delete-blogpost").Click();
+
+            cut.Find("#ok").Click();
+
+            repositoryMock.Verify(r => r.DeleteAsync(blogPostId), Times.Once);
+        }
+
+        [Fact]
+        public void ShouldNotDeleteBlogPostWhenCancelClicked()
+        {
+            const string blogPostId = "2";
+            var repositoryMock = new Mock<IRepository>();
+            using var ctx = new TestContext();
+            ctx.AddTestAuthorization().SetAuthorized("s");
+            ctx.Services.AddSingleton(repositoryMock.Object);
+            ctx.Services.AddSingleton(new Mock<IToastService>().Object);
+            var cut = ctx.RenderComponent<BlogPostAdminActions>(s => s.Add(p => p.BlogPostId, blogPostId));
+            cut.Find("#delete-blogpost").Click();
+
+            cut.Find("#cancel").Click();
+
+            repositoryMock.Verify(r => r.DeleteAsync(blogPostId), Times.Never);
+        }
+
+        [Fact]
+        public void ShouldGoToEditPageOnEditClick()
+        {
+            const string blogPostId = "2";
+            var repositoryMock = new Mock<IRepository>();
+            using var ctx = new TestContext();
+            ctx.AddTestAuthorization().SetAuthorized("s");
+            ctx.Services.AddSingleton(repositoryMock.Object);
+            ctx.Services.AddSingleton(new Mock<IToastService>().Object);
+            var navigationManager = ctx.Services.GetRequiredService<NavigationManager>();
+            var cut = ctx.RenderComponent<BlogPostAdminActions>(s => s.Add(p => p.BlogPostId, blogPostId));
+
+            cut.Find("#edit-blogpost").Click();
+
+            navigationManager.Uri.Should().EndWith($"update/{blogPostId}");
+        }
+    }
+}
---
+++ LinkDotNet.Blog.IntegrationTests/Web/Shared/ConfirmDialogTests.cs
@@ -0,0 +1,25 @@
+﻿using Bunit;
+using FluentAssertions;
+using LinkDotNet.Blog.Web.Shared;
+using Xunit;
+
+namespace LinkDotNet.Blog.IntegrationTests.Web.Shared
+{
+    public class ConfirmDialogTests
+    {
+        [Fact]
+        public void ShouldInvokeEventOnOkClick()
+        {
+            var okWasClicked = false;
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<ConfirmDialog>(
+                b => b
+                    .Add(p => p.OnYesPressed, _ => okWasClicked = true));
+            cut.Instance.Open();
+
+            cut.Find("#ok").Click();
+
+            okWasClicked.Should().BeTrue();
+        }
+    }
+}
