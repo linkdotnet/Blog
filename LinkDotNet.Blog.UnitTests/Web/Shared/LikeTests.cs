54b940eb11db785acb3904f0fc111d03852c0fea
Steven Giesel <stgiesel35@gmail.com>
2021-07-08 19:23:14 +0200

    Added Like Tests

--- LinkDotNet.Blog.UnitTests/Web/Shared/LikeTests.cs
+++ LinkDotNet.Blog.UnitTests/Web/Shared/LikeTests.cs
@@ -26,5 +26,76 @@ namespace LinkDotNet.Blog.UnitTests.Web.Shared
 
             label.Should().Be(expectedText);
         }
+
+        [Fact]
+        public void ShouldInvokeEventWhenButtonClicked()
+        {
+            Services.AddScoped(_ => new Mock<ILocalStorageService>().Object);
+            var blogPost = new BlogPostBuilder().Build();
+            var wasClicked = false;
+            var wasLike = false;
+            var cut = RenderComponent<Like>(
+                p => p.Add(l => l.BlogPost, blogPost)
+                    .Add(l => l.OnBlogPostLiked, b =>
+                    {
+                        wasClicked = true;
+                        wasLike = b;
+                    }));
+
+            cut.Find("button").Click();
+
+            wasClicked.Should().BeTrue();
+            wasLike.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShouldSetLocalStorageVariableOnClick()
+        {
+            var localStorage = new Mock<ILocalStorageService>();
+            Services.AddScoped(_ => localStorage.Object);
+            var blogPost = new BlogPostBuilder().Build();
+            var cut = RenderComponent<Like>(
+                p => p.Add(l => l.BlogPost, blogPost));
+
+            cut.Find("button").Click();
+
+            localStorage.Verify(l => l.SetItemAsync("hasLiked", true, default), Times.Once);
+        }
+
+        [Fact]
+        public void ShouldCheckLocalStorageOnInit()
+        {
+            var localStorage = new Mock<ILocalStorageService>();
+            localStorage.Setup(l => l.ContainKeyAsync("hasLiked", default)).ReturnsAsync(true);
+            localStorage.Setup(l => l.GetItemAsync<bool>("hasLiked", default)).ReturnsAsync(true);
+            Services.AddScoped(_ => localStorage.Object);
+            var blogPost = new BlogPostBuilder().Build();
+            var wasLike = true;
+            var cut = RenderComponent<Like>(
+                p => p.Add(l => l.BlogPost, blogPost)
+                    .Add(l => l.OnBlogPostLiked, b => wasLike = b));
+
+            cut.Find("button").Click();
+
+            wasLike.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldCheckStorageOnClickAgainAndDoNothingOnMismatch()
+        {
+            var localStorage = new Mock<ILocalStorageService>();
+            Services.AddScoped(_ => localStorage.Object);
+            var blogPost = new BlogPostBuilder().Build();
+            var wasClicked = false;
+            var cut = RenderComponent<Like>(
+                p => p.Add(l => l.BlogPost, blogPost)
+                    .Add(l => l.OnBlogPostLiked, _ => wasClicked = true));
+            localStorage.Setup(l => l.ContainKeyAsync("hasLiked", default)).ReturnsAsync(true);
+            localStorage.Setup(l => l.GetItemAsync<bool>("hasLiked", default)).ReturnsAsync(true);
+
+            cut.Find("button").Click();
+
+            wasClicked.Should().BeFalse();
+        }
     }
 }
