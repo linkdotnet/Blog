f1f6154048b30ad06e15c0103df3acd422b85336
Steven Giesel <stgiesel35@gmail.com>
2021-07-16 10:59:24 +0200

    Added infrastructure for ravendb tests

---
+++ LinkDotNet.Blog.IntegrationTests/Infrastructure/Persistence/RavenDb/BlogPostRepositoryTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LinkDotNet.Infrastructure.Persistence.RavenDb;
+using Raven.Client.Documents;
+using Xunit;
+
+namespace LinkDotNet.Blog.IntegrationTests.Infrastructure.Persistence.RavenDb
+{
+    public sealed class BlogPostRepositoryTests : IDisposable
+    {
+        private readonly DocumentStore documentStore;
+        private readonly BlogPostRepository blogPostRepository;
+
+        public BlogPostRepositoryTests()
+        {
+            documentStore = new DocumentStore
+            {
+                Urls = new[] { "https://localhost:8080" },
+            };
+            documentStore.Initialize();
+
+            blogPostRepository = new BlogPostRepository(documentStore);
+        }
+
+        [Fact]
+        public async Task ShouldGetSomething()
+        {
+            var blogPosts = await blogPostRepository.GetAllAsync();
+
+            blogPosts.Should().NotBeNull();
+        }
+
+        public void Dispose()
+        {
+            documentStore?.Dispose();
+        }
+    }
+}
