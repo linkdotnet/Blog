cb4ed2075676ba8571a9310674e6a44efa912ac9
Steven Giesel <stgiesel35@gmail.com>
2021-07-17 10:09:26 +0200

    Added trim behaviour and tests for searchinput

---
+++ LinkDotNet.Blog.UnitTests/Web/Shared/SearchInputTests.cs
@@ -0,0 +1,49 @@
+﻿using Bunit;
+using FluentAssertions;
+using LinkDotNet.Blog.Web.Shared;
+using Xunit;
+
+namespace LinkDotNet.Blog.UnitTests.Web.Shared
+{
+    public class SearchInputTests : TestContext
+    {
+        [Fact]
+        public void ShouldReturnEnteredText()
+        {
+            var enteredString = string.Empty;
+            var cut = RenderComponent<SearchInput>(p => p.Add(s => s.SearchEntered, s => enteredString = s));
+            cut.Find("input").Change("Test");
+
+            cut.Find("button").Click();
+
+            enteredString.Should().Be("Test");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("\t")]
+        public void ShouldNotReturnValueWhenOnlyWhitespaces(string input)
+        {
+            var wasInvoked = false;
+            var cut = RenderComponent<SearchInput>(p => p.Add(s => s.SearchEntered, _ => wasInvoked = true));
+            cut.Find("input").Change(input);
+
+            cut.Find("button").Click();
+
+            wasInvoked.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldTrimData()
+        {
+            var enteredString = string.Empty;
+            var cut = RenderComponent<SearchInput>(p => p.Add(s => s.SearchEntered, s => enteredString = s));
+            cut.Find("input").Change("   Test 1 ");
+
+            cut.Find("button").Click();
+
+            enteredString.Should().Be("Test 1");
+        }
+    }
+}
