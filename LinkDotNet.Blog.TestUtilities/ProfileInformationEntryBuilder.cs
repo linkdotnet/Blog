3d01b426c359002b5b61d92cf15decfa3a1f3e79
Steven Giesel <stgiesel35@gmail.com>
2021-07-26 20:23:43 +0200

    Added Test and Buulder for ProfileInformationEntry

---
+++ LinkDotNet.Blog.TestUtilities/ProfileInformationEntryBuilder.cs
@@ -0,0 +1,27 @@
+﻿using LinkDotNet.Domain;
+
+namespace LinkDotNet.Blog.TestUtilities
+{
+    public class ProfileInformationEntryBuilder
+    {
+        private string key = "Key";
+        private string value = "Value";
+
+        public ProfileInformationEntryBuilder WithKey(string key)
+        {
+            this.key = key;
+            return this;
+        }
+
+        public ProfileInformationEntryBuilder WithValue(string value)
+        {
+            this.value = value;
+            return this;
+        }
+
+        public ProfileInformationEntry Build()
+        {
+            return ProfileInformationEntry.Create(key, value);
+        }
+    }
+}
---
+++ LinkDotNet.Blog.UnitTests/Domain/ProfileInformationEntryTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentAssertions;
+using LinkDotNet.Domain;
+using Xunit;
+
+namespace LinkDotNet.Blog.UnitTests.Domain
+{
+    public class ProfileInformationEntryTests
+    {
+        [Fact]
+        public void ShouldCreateObject()
+        {
+            var result = ProfileInformationEntry.Create("key", "value");
+
+            result.Key.Should().Be("key");
+            result.Value.Should().Be("value");
+        }
+
+        [Theory]
+        [InlineData("", "V")]
+        [InlineData(" ", "V")]
+        [InlineData(null, "V")]
+        [InlineData("K", "")]
+        [InlineData("K", " ")]
+        [InlineData("K", null)]
+        public void ShouldThrowExceptionWhenEmptyKeyOrValue(string key, string value)
+        {
+            Action act = () => ProfileInformationEntry.Create(key, value);
+
+            act.Should().Throw<ArgumentOutOfRangeException>();
+        }
+    }
+}
