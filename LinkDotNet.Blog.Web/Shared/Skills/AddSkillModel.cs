4a29f3fb81900442518a12067ddf24560d615081
Steven Giesel <stgiesel35@gmail.com>
2021-07-30 16:16:33 +0200

    Extended SkillTable

---
+++ LinkDotNet.Blog.Web/Shared/Skills/AddSkillModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkDotNet.Blog.Web.Shared.Skills
+{
+    public class AddSkillModel
+    {
+        [Required]
+        public string Skill { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public string Proficiency { get; set; }
+    }
+}
