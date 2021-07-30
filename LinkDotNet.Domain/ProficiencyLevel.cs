6fd24c45e5c6109e3524f9f1d357c960838c6d2a
Steven Giesel <stgiesel35@gmail.com>
2021-07-30 14:02:48 +0200

    Add skill table components

---
+++ LinkDotNet.Domain/ProficiencyLevel.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkDotNet.Domain
+{
+    public class ProficiencyLevel : Enumeration<ProficiencyLevel>
+    {
+        public static readonly ProficiencyLevel Familiar = new(nameof(Familiar));
+        public static readonly ProficiencyLevel Proficient = new(nameof(Proficient));
+        public static readonly ProficiencyLevel Expert = new(nameof(Expert));
+
+        public ProficiencyLevel(string key)
+            : base(key)
+        {
+        }
+    }
+}
---
+++ LinkDotNet.Domain/Skill.cs
@@ -0,0 +1,13 @@
+﻿namespace LinkDotNet.Domain
+{
+    public class Skill
+    {
+        public string IconUrl { get; set; }
+
+        public string Name { get; set; }
+
+        public string Capability { get; set; }
+
+        public ProficiencyLevel ProficiencyLevel { get; set; }
+    }
+}
