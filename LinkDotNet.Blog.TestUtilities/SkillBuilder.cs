4d73d1b6619e8d73358ee75ef7d61fd8fd86d073
Steven Giesel <stgiesel35@gmail.com>
2021-07-30 23:16:27 +0200

    Added Builder for Skills and added SkillTagTests

---
+++ LinkDotNet.Blog.TestUtilities/SkillBuilder.cs
@@ -0,0 +1,41 @@
+﻿using LinkDotNet.Domain;
+
+namespace LinkDotNet.Blog.TestUtilities
+{
+    public class SkillBuilder
+    {
+        private string skill = "C#";
+        private string iconUrl;
+        private string capability = "Backend";
+        private ProficiencyLevel proficiencyLevel = ProficiencyLevel.Familiar;
+
+        public SkillBuilder WithSkillName(string skill)
+        {
+            this.skill = skill;
+            return this;
+        }
+
+        public SkillBuilder WithIconUrl(string iconUrl)
+        {
+            this.iconUrl = iconUrl;
+            return this;
+        }
+
+        public SkillBuilder WithCapability(string capability)
+        {
+            this.capability = capability;
+            return this;
+        }
+
+        public SkillBuilder WithProficiencyLevel(ProficiencyLevel proficiencyLevel)
+        {
+            this.proficiencyLevel = proficiencyLevel;
+            return this;
+        }
+
+        public Skill Build()
+        {
+            return Skill.Create(skill, iconUrl, capability, proficiencyLevel.Key);
+        }
+    }
+}
---
+++ LinkDotNet.Blog.UnitTests/Web/Shared/Skills/SkillTagTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Bunit;
+using FluentAssertions;
+using LinkDotNet.Blog.TestUtilities;
+using LinkDotNet.Blog.Web.Shared.Skills;
+using Xunit;
+
+namespace LinkDotNet.Blog.UnitTests.Web.Shared.Skills
+{
+    public class SkillTagTests : TestContext
+    {
+        [Fact]
+        public void ShouldRenderImageAndText()
+        {
+            var skill = new SkillBuilder().WithSkillName("C#").WithIconUrl("test").Build();
+
+            var cut = RenderComponent<SkillTag>(p => p.Add(
+                s => s.Skill, skill));
+
+            cut.Find("span").TextContent.Should().Contain("C#");
+            cut.Find("img").Attributes.Single(a => a.Name == "src").Value.Should().Be("test");
+        }
+
+        [Fact]
+        public void ShouldNotRenderImageWhenNotAvailable()
+        {
+            var skill = new SkillBuilder().WithIconUrl(null).Build();
+
+            var cut = RenderComponent<SkillTag>(p => p.Add(
+                s => s.Skill, skill));
+
+            cut.FindAll("img").Should().HaveCount(0);
+        }
+    }
+}
