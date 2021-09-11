bd1d661d034b74af16bdf7f96020b26d0eaf5675
Steven Giesel <stgiesel35@gmail.com>
2021-09-11 12:25:28 +0200

    Remove obsolete class and added test

---
+++ LinkDotNet.Blog.IntegrationTests/Web/Pages/Admin/CreateNewBlogPostPageTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Blazored.Toast.Services;
+using Bunit;
+using Bunit.TestDoubles;
+using FluentAssertions;
+using LinkDotNet.Blog.Web.Pages.Admin;
+using LinkDotNet.Blog.Web.Shared.Admin;
+using LinkDotNet.Domain;
+using LinkDotNet.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace LinkDotNet.Blog.IntegrationTests.Web.Pages.Admin
+{
+    public class CreateNewBlogPostPageTests : SqlDatabaseTestBase<BlogPost>
+    {
+        [Fact]
+        public async Task ShouldSaveBlogPostOnSave()
+        {
+            using var ctx = new TestContext();
+            var toastService = new Mock<IToastService>();
+            ctx.AddTestAuthorization().SetAuthorized("some username");
+            ctx.Services.AddScoped<IRepository<BlogPost>>(_ => Repository);
+            ctx.Services.AddScoped(_ => toastService.Object);
+            using var cut = ctx.RenderComponent<CreateNewBlogPostPage>();
+            var newBlogPost = cut.FindComponent<CreateNewBlogPost>();
+
+            TriggerNewBlogPost(newBlogPost);
+
+            var blogPostFromDb = await DbContext.BlogPosts.SingleOrDefaultAsync(t => t.Title == "My Title");
+            blogPostFromDb.Should().NotBeNull();
+            blogPostFromDb.ShortDescription.Should().Be("My short Description");
+            toastService.Verify(t => t.ShowInfo("Created BlogPost My Title", string.Empty), Times.Once);
+        }
+
+        private void TriggerNewBlogPost(IRenderedComponent<CreateNewBlogPost> cut)
+        {
+            cut.Find("#title").Change("My Title");
+            cut.Find("#short").Change("My short Description");
+            cut.Find("#content").Change("My content");
+            cut.Find("#preview").Change("My preview url");
+            cut.Find("#published").Change(false);
+            cut.Find("#tags").Change("Tag1,Tag2,Tag3");
+
+            cut.Find("form").Submit();
+        }
+    }
+}
