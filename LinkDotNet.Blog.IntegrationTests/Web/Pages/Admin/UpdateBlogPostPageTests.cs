103a2d89a7720f396fe64e4dff10b52ad2fb91d8
Steven Giesel <stgiesel35@gmail.com>
2021-09-12 17:39:34 +0200

    Added more tests

---
+++ LinkDotNet.Blog.IntegrationTests/Web/Pages/Admin/UpdateBlogPostPageTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Blazored.Toast.Services;
+using Bunit;
+using Bunit.TestDoubles;
+using FluentAssertions;
+using LinkDotNet.Blog.TestUtilities;
+using LinkDotNet.Blog.Web.Pages.Admin;
+using LinkDotNet.Blog.Web.Shared.Admin;
+using LinkDotNet.Domain;
+using LinkDotNet.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace LinkDotNet.Blog.IntegrationTests.Web.Pages.Admin
+{
+    public class UpdateBlogPostPageTests : SqlDatabaseTestBase<BlogPost>
+    {
+        [Fact]
+        public async Task ShouldSaveBlogPostOnSave()
+        {
+            using var ctx = new TestContext();
+            var toastService = new Mock<IToastService>();
+            var blogPost = new BlogPostBuilder().WithTitle("Title").WithShortDescription("Sub").Build();
+            await Repository.StoreAsync(blogPost);
+            ctx.AddTestAuthorization().SetAuthorized("some username");
+            ctx.Services.AddScoped<IRepository<BlogPost>>(_ => Repository);
+            ctx.Services.AddScoped(_ => toastService.Object);
+            using var cut = ctx.RenderComponent<UpdateBlogPostPage>(
+                p => p.Add(s => s.BlogPostId, blogPost.Id));
+            var newBlogPost = cut.FindComponent<CreateNewBlogPost>();
+
+            TriggerUpdate(newBlogPost);
+
+            var blogPostFromDb = await DbContext.BlogPosts.SingleOrDefaultAsync(t => t.Id == blogPost.Id);
+            blogPostFromDb.Should().NotBeNull();
+            blogPostFromDb.ShortDescription.Should().Be("My new Description");
+            toastService.Verify(t => t.ShowInfo("Updated BlogPost Title", string.Empty), Times.Once);
+        }
+
+        [Fact]
+        public void ShouldThrowWhenNoIdProvided()
+        {
+            using var ctx = new TestContext();
+            ctx.AddTestAuthorization().SetAuthorized("some username");
+            ctx.Services.AddScoped<IRepository<BlogPost>>(_ => Repository);
+            ctx.Services.AddScoped(_ => new Mock<IToastService>().Object);
+
+            Action act = () => ctx.RenderComponent<UpdateBlogPostPage>(
+                p => p.Add(s => s.BlogPostId, null));
+
+            act.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        private static void TriggerUpdate(IRenderedFragment cut)
+        {
+            cut.Find("#short").Change("My new Description");
+
+            cut.Find("form").Submit();
+        }
+    }
+}
---
+++ LinkDotNet.Blog.UnitTests/Web/Pages/Admin/DashboardTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Bunit;
+using Bunit.TestDoubles;
+using FluentAssertions;
+using LinkDotNet.Blog.Web;
+using LinkDotNet.Blog.Web.Pages.Admin;
+using LinkDotNet.Blog.Web.Shared.Admin.Dashboard;
+using LinkDotNet.Domain;
+using LinkDotNet.Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace LinkDotNet.Blog.UnitTests.Web.Pages.Admin
+{
+    public class DashboardTests : TestContext
+    {
+        [Fact]
+        public void ShouldNotShowAboutMeStatisticsWhenDisabled()
+        {
+            var dashboardService = new Mock<IDashboardService>();
+            this.AddTestAuthorization().SetAuthorized("test");
+            Services.AddScoped(_ => CreateAppConfiguration(false));
+            Services.AddScoped(_ => dashboardService.Object);
+            Services.AddScoped(_ => new Mock<IRepository<BlogPost>>().Object);
+            dashboardService.Setup(d => d.GetDashboardDataAsync())
+                .ReturnsAsync(new DashboardData());
+
+            var cut = RenderComponent<Dashboard>();
+
+            cut.FindComponents<DashboardCard>()
+                .Any(c => c.Instance.Text == "About Me:")
+                .Should()
+                .BeFalse();
+        }
+
+        private static AppConfiguration CreateAppConfiguration(bool aboutMeEnabled)
+        {
+            return new AppConfiguration
+            {
+                IsAboutMeEnabled = aboutMeEnabled,
+            };
+        }
+    }
+}
