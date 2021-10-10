b3441c90d866d6576a4a4241aa64fd0a19df8a49
Steven Giesel <stgiesel35@gmail.com>
2021-10-10 18:08:57 +0200

    Added Test for XmlFileWriter

---
+++ LinkDotNet.Blog.UnitTests/Web/Shared/Services/XmlFileWriterTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LinkDotNet.Blog.Web.Shared.Services;
+using Xunit;
+
+namespace LinkDotNet.Blog.UnitTests.Web.Shared.Services
+{
+    public sealed class XmlFileWriterTests : IDisposable
+    {
+        private const string OutputFilename = "somefile.txt";
+
+        [Fact]
+        public async Task ShouldWriteToFile()
+        {
+            var myObj = new MyObject { Property = "Prop" };
+
+            await new XmlFileWriter().WriteObjectToXmlFileAsync(myObj, OutputFilename);
+
+            var content = await File.ReadAllTextAsync(OutputFilename);
+            content.Should().NotBeNull();
+            content.Should().Contain("<MyObject");
+            content.Should().Contain("<Property>Prop</Property>");
+        }
+
+        public void Dispose()
+        {
+            if (File.Exists(OutputFilename))
+            {
+                File.Delete(OutputFilename);
+            }
+        }
+
+        public class MyObject
+        {
+            public string Property { get; set; }
+        }
+    }
+}
