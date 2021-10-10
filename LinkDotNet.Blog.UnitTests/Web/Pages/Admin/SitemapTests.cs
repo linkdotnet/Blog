983e7aeac96c76cda2e9d5e45ab14ab40b6a620b
Steven Giesel <stgiesel35@gmail.com>
2021-10-10 18:30:55 +0200

    Added Test for Sitemap

---
+++ LinkDotNet.Blog.UnitTests/Web/Pages/Admin/SitemapTests.cs
@@ -0,0 +1,29 @@
+﻿using Bunit;
+using Bunit.TestDoubles;
+using LinkDotNet.Blog.Web.Pages.Admin;
+using LinkDotNet.Blog.Web.Shared.Services.Sitemap;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace LinkDotNet.Blog.UnitTests.Web.Pages.Admin
+{
+    public class SitemapTests : TestContext
+    {
+        [Fact]
+        public void ShouldDisplayAndSaveSitemap()
+        {
+            this.AddTestAuthorization().SetAuthorized("steven");
+            var sitemapMock = new Mock<ISitemapService>();
+            Services.AddScoped(_ => sitemapMock.Object);
+            var sitemap = new SitemapUrlSet();
+            sitemapMock.Setup(s => s.CreateSitemapAsync())
+                .ReturnsAsync(sitemap);
+            var cut = RenderComponent<Sitemap>();
+
+            cut.Find("button").Click();
+
+            sitemapMock.Verify(s => s.SaveSitemapToFileAsync(sitemap));
+        }
+    }
+}
