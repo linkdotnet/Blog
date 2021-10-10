1f3782fce61ba99e09c32074a4ac918df0bd2cd9
Steven Giesel <stgiesel35@gmail.com>
2021-10-10 20:49:34 +0200

    Added test for table

--- LinkDotNet.Blog.UnitTests/Web/Pages/Admin/SitemapTests.cs
+++ LinkDotNet.Blog.UnitTests/Web/Pages/Admin/SitemapTests.cs
@@ -1,5 +1,8 @@
-﻿using Bunit;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bunit;
 using Bunit.TestDoubles;
+using FluentAssertions;
 using LinkDotNet.Blog.Web.Pages.Admin;
 using LinkDotNet.Blog.Web.Shared.Services.Sitemap;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,7 +14,7 @@ namespace LinkDotNet.Blog.UnitTests.Web.Pages.Admin
     public class SitemapTests : TestContext
     {
         [Fact]
-        public void ShouldDisplayAndSaveSitemap()
+        public void ShouldSaveSitemap()
         {
             this.AddTestAuthorization().SetAuthorized("steven");
             var sitemapMock = new Mock<ISitemapService>();
@@ -24,6 +27,31 @@ namespace LinkDotNet.Blog.UnitTests.Web.Pages.Admin
             cut.Find("button").Click();
 
             sitemapMock.Verify(s => s.SaveSitemapToFileAsync(sitemap));
+        }
+
+        [Fact]
+        public void ShouldDisplaySitemap()
+        {
+            this.AddTestAuthorization().SetAuthorized("steven");
+            var sitemapMock = new Mock<ISitemapService>();
+            Services.AddScoped(_ => sitemapMock.Object);
+            var sitemap = new SitemapUrlSet
+            {
+                Urls = new List<SitemapUrl>
+                {
+                    new() { Location = "loc", LastModified = "Now" },
+                },
+            };
+            sitemapMock.Setup(s => s.CreateSitemapAsync())
+                .ReturnsAsync(sitemap);
+            var cut = RenderComponent<Sitemap>();
+
+            cut.Find("button").Click();
+
+            cut.WaitForState(() => cut.FindAll("tr").Count > 1);
+            var row = cut.FindAll("tr").Last();
+            row.Children.First().InnerHtml.Should().Be("loc");
+            row.Children.Last().InnerHtml.Should().Be("Now");
         }
     }
 }
