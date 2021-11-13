ecbafb027fbbe99a134433ed90d7156eef5c1e77
Steven Giesel <stgiesel35@gmail.com>
2021-11-13 19:40:06 +0100

    Fixed reset of filter

--- LinkDotNet.Blog.UnitTests/Web/Shared/Admin/Dashboard/DateRangeSelectorTests.cs
+++ LinkDotNet.Blog.UnitTests/Web/Shared/Admin/Dashboard/DateRangeSelectorTests.cs
@@ -39,4 +39,22 @@ public class DateRangeSelectorTests : TestContext
         filter.EndDate.Should().Be(new DateTime(2020, 1, 1));
         filter.StartDate.Should().BeNull();
     }
+
+    [Fact]
+    public void ShouldReset()
+    {
+        Filter filter = null;
+        var cut = RenderComponent<DateRangeSelector>(p => p.Add(s => s.FilterChanged, f =>
+        {
+            filter = f;
+        }));
+        cut.Find("#startDate").Change(new DateTime(2020, 1, 1));
+        cut.Find("#endDate").Change(new DateTime(2020, 1, 1));
+
+        cut.Find("#startDate").Change(string.Empty);
+        cut.Find("#endDate").Change(string.Empty);
+
+        filter.StartDate.Should().BeNull();
+        filter.EndDate.Should().BeNull();
+    }
 }
