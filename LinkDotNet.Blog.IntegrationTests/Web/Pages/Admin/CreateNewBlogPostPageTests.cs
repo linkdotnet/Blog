b4ee5b26c20bbb36ed517b8a61950715223b688f
Steven Giesel <stgiesel35@gmail.com>
2021-11-26 08:19:53 +0100

    Use bundled and minified css only for release builds for easier debugging and hot reload

--- LinkDotNet.Blog.IntegrationTests/Web/Pages/Admin/CreateNewBlogPostPageTests.cs
+++ LinkDotNet.Blog.IntegrationTests/Web/Pages/Admin/CreateNewBlogPostPageTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Blazored.Toast.Services;
 using Bunit;
 using Bunit.TestDoubles;
---
+++ LinkDotNet.Blog.Web/Pages/DebugHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace LinkDotNet.Blog.Web.Pages;
+
+public static class DebugHelper
+{
+#if DEBUG
+    public const bool IsDebug = true;
+#else
+    public const bool IsDebug = false;
+#endif
+
+}
