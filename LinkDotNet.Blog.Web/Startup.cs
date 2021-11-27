e6ae83483000a8a22c513f4bccf3e6784f61cd18
Steven Giesel <stgiesel35@gmail.com>
2021-11-27 16:36:46 +0100

    Use built-in meta and title update

--- LinkDotNet.Blog.Web/Startup.cs
+++ LinkDotNet.Blog.Web/Startup.cs
@@ -57,8 +57,6 @@ public class Startup
             app.UseHsts();
         }
 
-        app.UseHeadElementServerPrerendering();
-
         app.UseHttpsRedirection();
         app.UseStaticFiles();
 
