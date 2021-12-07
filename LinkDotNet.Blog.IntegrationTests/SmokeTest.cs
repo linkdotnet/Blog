747abc7173851361587f116018d37ebe47423cd7
Steven Giesel <stgiesel35@gmail.com>
2021-12-07 19:01:22 +0100

    Added Smoke Test

---
+++ LinkDotNet.Blog.IntegrationTests/SmokeTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using LinkDotNet.Blog.Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace LinkDotNet.Blog.IntegrationTests
+{
+    public class SmokeTest
+    {
+        [Fact]
+        public async Task ShouldBootUpApplication()
+        {
+            var application = new WebApplicationFactory<Program>().WithWebHostBuilder(b => { });
+            var client = application.CreateClient();
+
+            var result = await client.GetAsync("/");
+
+            result.IsSuccessStatusCode.Should().BeTrue();
+        }
+    }
+}
