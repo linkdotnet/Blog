931b8a35545ac1eb1e9958229d2a3607f1f7193f
Steven Giesel <stgiesel35@gmail.com>
2021-12-08 09:23:38 +0100

    Added ProfiencyLevel tests

---
+++ LinkDotNet.Blog.UnitTests/Domain/ProficiencyLevelTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentAssertions;
+using LinkDotNet.Blog.Domain;
+using Xunit;
+
+namespace LinkDotNet.Blog.UnitTests.Domain
+{
+    public class ProficiencyLevelTests
+    {
+        [Theory]
+        [InlineData("Familiar")]
+        [InlineData("Proficient")]
+        [InlineData("Expert")]
+        public void ShouldCreateValidLevels(string key)
+        {
+            var level = ProficiencyLevel.Create(key);
+
+            level.Should().NotBeNull();
+        }
+
+        [Theory]
+        [InlineData("NotALevel")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void ShouldNotCreateInvalidLevels(string key)
+        {
+            Action act = () => ProficiencyLevel.Create(key);
+
+            act.Should().Throw<Exception>();
+        }
+    }
+}
