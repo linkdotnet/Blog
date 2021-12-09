79a6b8c0a54b942b0a5f1e99441bc50614611dfe
Steven Giesel <stgiesel35@gmail.com>
2021-12-09 19:08:51 +0100

    Added SmokeTest for SQL

---
+++ LinkDotNet.Blog.IntegrationTests/SmokeTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using LinkDotNet.Blog.Infrastructure.Persistence;
+using LinkDotNet.Blog.Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace LinkDotNet.Blog.IntegrationTests;
+
+public class SmokeTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> factory;
+
+    public SmokeTests(WebApplicationFactory<Program> factory)
+    {
+        this.factory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.UseSetting("PersistenceProvider", PersistenceProvider.InMemory.Key);
+        });
+    }
+
+    [Fact]
+    public async Task ShouldBootUpApplication()
+    {
+        var client = factory.CreateClient();
+
+        var result = await client.GetAsync("/");
+
+        result.IsSuccessStatusCode.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task ShouldBootUpWithSqlDatabase()
+    {
+        var client = factory.WithWebHostBuilder(builder =>
+        {
+            builder.UseSetting("PersistenceProvider", PersistenceProvider.SqliteServer.Key);
+            builder.UseSetting("ConnectionString", "DataSource=file::memory:?cache=shared");
+        }).CreateClient();
+
+        var result = await client.GetAsync("/");
+
+        result.IsSuccessStatusCode.Should().BeTrue();
+    }
+}
