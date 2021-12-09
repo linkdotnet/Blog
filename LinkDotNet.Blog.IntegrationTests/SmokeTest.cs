1de9c2c972439828be7babb6d721d013dd02077d
Steven Giesel <stgiesel35@gmail.com>
2021-12-09 08:27:44 +0100

    File-scoped namespace for SmokeTest

--- LinkDotNet.Blog.IntegrationTests/SmokeTest.cs
+++ LinkDotNet.Blog.IntegrationTests/SmokeTest.cs
@@ -4,28 +4,27 @@ using LinkDotNet.Blog.Web;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
-namespace LinkDotNet.Blog.IntegrationTests
+namespace LinkDotNet.Blog.IntegrationTests;
+
+public class SmokeTest : IClassFixture<WebApplicationFactory<Program>>
 {
-    public class SmokeTest : IClassFixture<WebApplicationFactory<Program>>
+    private readonly WebApplicationFactory<Program> factory;
+
+    public SmokeTest(WebApplicationFactory<Program> factory)
     {
-        private readonly WebApplicationFactory<Program> factory;
-
-        public SmokeTest(WebApplicationFactory<Program> factory)
+        this.factory = factory.WithWebHostBuilder(builder =>
         {
-            this.factory = factory.WithWebHostBuilder(builder =>
-            {
-                builder.UseSetting("PersistenceProvider", "InMemory");
-            });
-        }
+            builder.UseSetting("PersistenceProvider", "InMemory");
+        });
+    }
 
-        [Fact]
-        public async Task ShouldBootUpApplication()
-        {
-            var client = factory.CreateClient();
+    [Fact]
+    public async Task ShouldBootUpApplication()
+    {
+        var client = factory.CreateClient();
 
-            var result = await client.GetAsync("/");
+        var result = await client.GetAsync("/");
 
-            result.IsSuccessStatusCode.Should().BeTrue();
-        }
+        result.IsSuccessStatusCode.Should().BeTrue();
     }
 }
