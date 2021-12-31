f579c5c493f759fa83016d984df1b6d5c8a5e415
Steven Giesel <stgiesel35@gmail.com>
2021-12-31 16:45:17 +0100

    Added Test for OgData component

---
+++ tests/LinkDotNet.Blog.UnitTests/Web/Shared/OgDataTests.cs
@@ -0,0 +1,46 @@
+using System.Linq;
+using AngleSharp.Html.Dom;
+using Bunit;
+using FluentAssertions;
+using LinkDotNet.Blog.Web.Shared;
+using Microsoft.AspNetCore.Components.Web;
+using Xunit;
+
+namespace LinkDotNet.Blog.UnitTests.Web.Shared;
+
+public class OgDataTests : TestContext
+{
+    [Fact]
+    public void ShouldRenderMetaTagsWhenSet()
+    {
+        ComponentFactories.AddStub<HeadContent>(ps => ps.Get(p => p.ChildContent));
+
+        var cut = RenderComponent<OgData>(p => p
+            .Add(s => s.Title, "Title")
+            .Add(s => s.Description, "Description")
+            .Add(s => s.AbsolutePreviewImageUrl, "http://localhost/image.png")
+            .Add(s => s.Keywords, "key1,key2"));
+
+        AssertMetaTagExistsWithValue(cut, "title", "Title", "og:title");
+        AssertMetaTagExistsWithValue(cut, "image", "http://localhost/image.png", "og:image");
+        AssertMetaTagExistsWithValue(cut, "keywords", "key1,key2");
+        AssertMetaTagExistsWithValue(cut, "description", "Description", "og:description");
+    }
+
+    private static void AssertMetaTagExistsWithValue(
+        IRenderedFragment cut,
+        string metaTag,
+        string metaTagValue,
+        string ogPropertyName = null)
+    {
+        var metaTags = cut.FindAll("meta");
+        var titleMeta = metaTags.SingleOrDefault(m => m.Attributes.Any(a => a.Value == metaTag));
+        titleMeta.Should().NotBeNull();
+        var titleMetaTag = (IHtmlMetaElement)titleMeta;
+        titleMetaTag.Content.Should().Be(metaTagValue);
+        if (ogPropertyName != null)
+        {
+            titleMetaTag.Attributes.Any(a => a.Value == ogPropertyName).Should().BeTrue();
+        }
+    }
+}
