55f606c43d3f779d708d3ed2cf3eb29870faea2b
Steven Giesel <stgiesel35@gmail.com>
2022-02-06 10:47:04 +0100

    Add possibility to generate coverage report locally

---
+++ runLocalCoverage.cmd
@@ -0,0 +1,3 @@
+dotnet test --collect:"XPlat Code Coverage" --no-build --verbosity normal /p:CollectCoverage=true /p:CoverletOutputFormat="opencover"
+dotnet tool install --global dotnet-reportgenerator-globaltool
+reportgenerator.exe -reports:".\tests\*\coverage.opencover.xml" -targetdir:".\CoverageReport" -reporttypes:"html"
---
+++ runLocalCoverage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dotnet test --collect:"XPlat Code Coverage" --no-build --verbosity normal -p:CollectCoverage=true -p:CoverletOutputFormat="opencover"
+dotnet tool install --global dotnet-reportgenerator-globaltool
+reportgenerator.exe -reports:".\tests\*\coverage.opencover.xml" -targetdir:"C:\Users\stgi\Desktop\report" -reporttypes:"htmlsummary"
