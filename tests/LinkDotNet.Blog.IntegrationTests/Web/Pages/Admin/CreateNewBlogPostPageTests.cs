d0e1f9ab6cf84d8d6c21f4b3813bb73bb6a0ab93
Steven Giesel <stgiesel35@gmail.com>
2022-02-06 19:00:56 +0100

    Added test for UploadFile content

--- tests/LinkDotNet.Blog.IntegrationTests/Web/Pages/Admin/CreateNewBlogPostPageTests.cs
+++ tests/LinkDotNet.Blog.IntegrationTests/Web/Pages/Admin/CreateNewBlogPostPageTests.cs
@@ -8,6 +8,7 @@ using LinkDotNet.Blog.Web.Pages.Admin;
 using LinkDotNet.Blog.Web.Shared;
 using LinkDotNet.Blog.Web.Shared.Admin;
 using LinkDotNet.Blog.Web.Shared.Services;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -36,6 +37,23 @@ public class CreateNewBlogPostPageTests : SqlDatabaseTestBase<BlogPost>
         toastService.Verify(t => t.ShowInfo("Created BlogPost My Title", string.Empty, null), Times.Once);
     }
 
+    [Fact]
+    public async Task ShouldSetContentFromFile()
+    {
+        using var ctx = new TestContext();
+        const string contentFromFile = "content";
+        ctx.AddTestAuthorization().SetAuthorized("some username");
+        ctx.Services.AddScoped<IRepository<BlogPost>>(_ => Repository);
+        ctx.Services.AddScoped(_ => Mock.Of<IToastService>());
+        var args = SetupUploadFile(contentFromFile, ctx);
+        var cut = ctx.RenderComponent<CreateNewBlogPost>();
+        var uploadFile = cut.FindComponent<UploadFile>();
+
+        await uploadFile.InvokeAsync(() => cut.FindComponent<InputFile>().Instance.OnChange.InvokeAsync(args));
+
+        cut.Find("#content").TextContent.Should().Be(contentFromFile);
+    }
+
     private static void TriggerNewBlogPost(IRenderedComponent<CreateNewBlogPost> cut)
     {
         cut.Find("#title").Input("My Title");
@@ -46,5 +64,20 @@ public class CreateNewBlogPostPageTests : SqlDatabaseTestBase<BlogPost>
         cut.Find("#tags").Change("Tag1,Tag2,Tag3");
 
         cut.Find("form").Submit();
+    }
+
+    private static InputFileChangeEventArgs SetupUploadFile(string contentFromFile, TestContext ctx)
+    {
+        var file = new Mock<IBrowserFile>();
+        var fileProcessor = new Mock<IFileProcessor>();
+        fileProcessor.Setup(f => f.GetContent(file.Object)).ReturnsAsync(contentFromFile);
+        var args = new InputFileChangeEventArgs(new[]
+        {
+            file.Object,
+        });
+        ctx.Services.AddScoped(_ => fileProcessor.Object);
+        ctx.JSInterop.SetupVoid(invocation => invocation.Identifier == "Blazor._internal.InputFile.init")
+            .SetVoidResult();
+        return args;
     }
 }
