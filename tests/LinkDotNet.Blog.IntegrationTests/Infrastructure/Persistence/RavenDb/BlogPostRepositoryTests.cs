afc93b1dd2a0f0516634f062573055ebea698f84
Steven Giesel <stgiesel35@gmail.com>
2022-02-08 20:48:10 +0100

    Set net version for RavenDb tests

--- tests/LinkDotNet.Blog.IntegrationTests/Infrastructure/Persistence/RavenDb/BlogPostRepositoryTests.cs
+++ tests/LinkDotNet.Blog.IntegrationTests/Infrastructure/Persistence/RavenDb/BlogPostRepositoryTests.cs
@@ -162,7 +162,7 @@ public sealed class BlogPostRepositoryTests : RavenTestDriver
             ConfigureServer(new TestServerOptions
             {
                 DataDirectory = "./RavenDbTest/",
-                FrameworkVersion = "6.0.1",
+                FrameworkVersion = "6.0.2",
             });
         }
     }
