51d1e45c348a9a4f03efe72ee0b7bcf810d345fe
Steven Giesel <stgiesel35@gmail.com>
2022-06-24 18:00:23 +0200

    Anchor element has no href when no link provided

---
+++ tests/LinkDotNet.Blog.UnitTests/Web/Features/Home/Components/AnchorTests.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using AngleSharp.Html.Dom;
+using AngleSharpWrappers;
+using Bunit;
+using LinkDotNet.Blog.Web.Features.Home.Components;
+
+namespace LinkDotNet.Blog.UnitTests.Web.Features.Home.Components;
+
+public class AnchorTests : TestContext
+{
+    [Fact]
+    public void ShouldShowHrefWhenNotEmpty()
+    {
+        var cut = RenderComponent<Anchor>(ps => ps
+            .Add(p => p.Href, "http://url/")
+            .Add(p => p.CssClass, "page"));
+
+        var anchor = cut.Find("a").Unwrap() as IHtmlAnchorElement;
+        anchor.Should().NotBeNull();
+        anchor.Href.Should().Be("http://url/");
+        anchor.GetAttribute("class").Should().Be("page");
+    }
+
+    [Fact]
+    public void ShouldNotShowHrefWhenEmpty()
+    {
+        var cut = RenderComponent<Anchor>(ps => ps
+            .Add(p => p.Href, string.Empty)
+            .Add(p => p.CssClass, "page"));
+
+        var anchor = cut.Find("a").Unwrap() as IHtmlAnchorElement;
+        anchor.Should().NotBeNull();
+        anchor.Attributes.Count(a => a.Name == "href").Should().Be(0);
+    }
+}
