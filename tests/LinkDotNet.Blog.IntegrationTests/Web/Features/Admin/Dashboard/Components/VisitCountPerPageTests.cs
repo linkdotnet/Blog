caf953bfb345860c3b68957a46da14b4571f9f47
Steven Giesel <stgiesel35@gmail.com>
2022-06-27 17:49:03 +0200

    Show total amount of clicks

--- tests/LinkDotNet.Blog.IntegrationTests/Web/Features/Admin/Dashboard/Components/VisitCountPerPageTests.cs
+++ tests/LinkDotNet.Blog.IntegrationTests/Web/Features/Admin/Dashboard/Components/VisitCountPerPageTests.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using AngleSharp.Html.Dom;
+using AngleSharpWrappers;
 using Bunit;
 using LinkDotNet.Blog.Domain;
 using LinkDotNet.Blog.TestUtilities;
@@ -66,6 +67,32 @@ public class VisitCountPerPageTests : SqlDatabaseTestBase<BlogPost>
         titleData.InnerHtml.Should().Be(blogPost1.Title);
         titleData.Href.Should().Contain($"blogPost/{blogPost1.Id}");
         elements[1].InnerHtml.Should().Be("1");
+    }
+
+    [Fact]
+    public async Task ShouldShowTotalClickCount()
+    {
+        var blogPost1 = new BlogPostBuilder().WithTitle("1").WithLikes(2).Build();
+        var blogPost2 = new BlogPostBuilder().WithTitle("2").WithLikes(2).Build();
+        await Repository.StoreAsync(blogPost1);
+        await Repository.StoreAsync(blogPost2);
+        var clicked1 = new UserRecord
+            { UrlClicked = $"blogPost/{blogPost1.Id}", DateTimeUtcClicked = new DateTime(2020, 1, 1) };
+        var clicked2 = new UserRecord
+            { UrlClicked = $"blogPost/{blogPost1.Id}", DateTimeUtcClicked = DateTime.MinValue };
+        var clicked3 = new UserRecord
+            { UrlClicked = $"blogPost/{blogPost2.Id}", DateTimeUtcClicked = DateTime.MinValue };
+        var clicked4 = new UserRecord
+            { UrlClicked = $"blogPost/{blogPost1.Id}", DateTimeUtcClicked = new DateTime(2021, 1, 1) };
+        await DbContext.UserRecords.AddRangeAsync(new[] { clicked1, clicked2, clicked3, clicked4 });
+        await DbContext.SaveChangesAsync();
+        using var ctx = new TestContext();
+        ctx.Services.AddScoped(_ => DbContext);
+
+        var cut = ctx.RenderComponent<VisitCountPerPage>();
+
+        cut.WaitForState(() => cut.FindAll("td").Any());
+        cut.Find("#total-clicks").Unwrap().TextContent.Should().Be("4 clicks in total");
     }
 
     private async Task SaveBlogPostArticleClicked(string blogPostId, int count)
