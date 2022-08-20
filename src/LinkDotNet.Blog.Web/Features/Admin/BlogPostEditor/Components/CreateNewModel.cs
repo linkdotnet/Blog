138dd50ce2b657164a9699ca32a02cb86842e720
Steven Giesel <stgiesel35@gmail.com>
2022-08-20 18:35:25 +0200

    feat: prevent navigation if blogpost is dirty

--- src/LinkDotNet.Blog.Web/Features/Admin/BlogPostEditor/Components/CreateNewModel.cs
+++ src/LinkDotNet.Blog.Web/Features/Admin/BlogPostEditor/Components/CreateNewModel.cs
@@ -7,57 +7,130 @@ namespace LinkDotNet.Blog.Web.Features.Admin.BlogPostEditor.Components;
 
 public class CreateNewModel
 {
-    public string Id { get; set; }
+    private DateTime originalUpdatedDate;
+    private string id;
+    private string title;
+    private string shortDescription;
+    private string content;
+    private string previewImageUrl;
+    private bool isPublished = true;
+    private bool shouldUpdateDate;
+    private string tags;
+    private string previewImageUrlFallback;
 
     [Required]
-    public string Title { get; set; }
+    public string Title
+    {
+        get => title;
+        set
+        {
+            title = value;
+            IsDirty = true;
+        }
+    }
 
     [Required]
-    public string ShortDescription { get; set; }
+    public string ShortDescription
+    {
+        get => shortDescription;
+        set
+        {
+            shortDescription = value;
+            IsDirty = true;
+        }
+    }
 
     [Required]
-    public string Content { get; set; }
+    public string Content
+    {
+        get => content;
+        set
+        {
+            content = value;
+            IsDirty = true;
+        }
+    }
 
     [Required]
-    public string PreviewImageUrl { get; set; }
+    public string PreviewImageUrl
+    {
+        get => previewImageUrl;
+        set
+        {
+            previewImageUrl = value;
+            IsDirty = true;
+        }
+    }
 
     [Required]
-    public bool IsPublished { get; set; } = true;
+    public bool IsPublished
+    {
+        get => isPublished;
+        set
+        {
+            isPublished = value;
+            IsDirty = true;
+        }
+    }
 
     [Required]
-    public bool ShouldUpdateDate { get; set; } = false;
+    public bool ShouldUpdateDate
+    {
+        get => shouldUpdateDate;
+        set
+        {
+            shouldUpdateDate = value;
+            IsDirty = true;
+        }
+    }
 
-    public string Tags { get; set; }
+    public string Tags
+    {
+        get => tags;
+        set
+        {
+            tags = value;
+            IsDirty = true;
+        }
+    }
 
-    public DateTime OriginalUpdatedDate { get; set; }
+    public string PreviewImageUrlFallback
+    {
+        get => previewImageUrlFallback;
+        set
+        {
+            previewImageUrlFallback = value;
+            IsDirty = true;
+        }
+    }
 
-    public string PreviewImageUrlFallback { get; set; }
+    public bool IsDirty { get; private set; }
 
     public static CreateNewModel FromBlogPost(BlogPost blogPost)
     {
         return new CreateNewModel
         {
-            Id = blogPost.Id,
+            id = blogPost.Id,
             Content = blogPost.Content,
             Tags = blogPost.Tags != null ? string.Join(",", blogPost.Tags.Select(t => t.Content)) : null,
             Title = blogPost.Title,
             ShortDescription = blogPost.ShortDescription,
             IsPublished = blogPost.IsPublished,
             PreviewImageUrl = blogPost.PreviewImageUrl,
-            OriginalUpdatedDate = blogPost.UpdatedDate,
+            originalUpdatedDate = blogPost.UpdatedDate,
             PreviewImageUrlFallback = blogPost.PreviewImageUrlFallback,
         };
     }
 
     public BlogPost ToBlogPost()
     {
-        var tags = string.IsNullOrWhiteSpace(Tags) ? ArraySegment<string>.Empty : Tags.Split(",");
-        DateTime? updatedDate = ShouldUpdateDate || OriginalUpdatedDate == default
+        var tagList = string.IsNullOrWhiteSpace(Tags) ? ArraySegment<string>.Empty : Tags.Split(",");
+        DateTime? updatedDate = ShouldUpdateDate || originalUpdatedDate == default
             ? null
-            : OriginalUpdatedDate;
+            : originalUpdatedDate;
 
-        var blogPost = BlogPost.Create(Title, ShortDescription, Content, PreviewImageUrl, IsPublished, updatedDate, tags, PreviewImageUrlFallback);
-        blogPost.Id = Id;
+        var blogPost = BlogPost.Create(Title, ShortDescription, Content, PreviewImageUrl, IsPublished, updatedDate, tagList, PreviewImageUrlFallback);
+        blogPost.Id = id;
         return blogPost;
     }
 }
