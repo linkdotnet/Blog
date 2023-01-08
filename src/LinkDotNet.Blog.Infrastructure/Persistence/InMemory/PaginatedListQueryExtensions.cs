e6f8d82fccf062aff966fbce766bfb279286e8ae
Steven Giesel <stgiesel35@gmail.com>
2023-01-08 21:38:32 +0100

    Remove unused method

--- src/LinkDotNet.Blog.Infrastructure/Persistence/InMemory/PaginatedListQueryExtensions.cs
+++ src/LinkDotNet.Blog.Infrastructure/Persistence/InMemory/PaginatedListQueryExtensions.cs
@@ -6,18 +6,6 @@ namespace LinkDotNet.Blog.Infrastructure.Persistence.InMemory;
 
 public static class PaginatedListQueryExtensions
 {
-    public static Task<IPaginatedList<T>> ToPagedList<T>(this IEnumerable<T> source)
-    {
-        var count = source.Count();
-        if (count > 0)
-        {
-            var items = source.ToList();
-            return Task.FromResult<IPaginatedList<T>>(new PaginatedList<T>(items, count, 1, int.MaxValue));
-        }
-
-        return Task.FromResult<IPaginatedList<T>>(PaginatedList<T>.Empty);
-    }
-
     public static Task<IPaginatedList<T>> ToPagedList<T>(this IEnumerable<T> source, int pageIndex, int pageSize)
     {
         var count = source.Count();
