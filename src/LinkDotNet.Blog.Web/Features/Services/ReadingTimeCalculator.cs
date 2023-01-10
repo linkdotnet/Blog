d28baa74eb8e7ae4f35607843594b475a679b68f
Steven Giesel <stgiesel35@gmail.com>
2023-01-10 08:00:53 +0100

    Use own class

---
+++ src/LinkDotNet.Blog.Web/Features/Services/ReadingTimeCalculator.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace LinkDotNet.Blog.Web.Features.Services;
+
+public static partial class ReadingTimeCalculator
+{
+    public static int CalculateReadingTime(string content)
+    {
+        const int wordsPerMinute = 250;
+        const double minutesPerImage = 0.5;
+
+        var imageCount = ImageRegex().Matches(content).Count;
+
+        var wordCount = GetWordCount(content);
+        var readTimeWords = wordCount / wordsPerMinute;
+        var readTimeImages = imageCount * minutesPerImage;
+        return (int)Math.Ceiling(readTimeWords + readTimeImages);
+    }
+
+    [GeneratedRegex(@"\!\[.*?\]\((.*?)\)")]
+    private static partial Regex ImageRegex();
+
+    private static int GetWordCount(ReadOnlySpan<char> content)
+    {
+        var wordCount = 0;
+        for (var i = 0; i < content.Length; i++)
+        {
+            if (content[i] == ' ')
+            {
+                wordCount++;
+            }
+        }
+
+        return wordCount;
+    }
+}
