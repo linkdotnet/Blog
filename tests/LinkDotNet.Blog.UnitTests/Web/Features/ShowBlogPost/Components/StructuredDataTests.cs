3d51ea607d767aa75ae8c923f7d7202b81005fa0
Steven Giesel <stgiesel35@gmail.com>
2023-07-04 16:35:42 +0200

    Use raw markup

---
+++ tests/LinkDotNet.Blog.UnitTests/Web/Features/ShowBlogPost/Components/StructuredDataTests.cs
@@ -0,0 +1,22 @@
+using LinkDotNet.Blog.Web.Features.ShowBlogPost.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace LinkDotNet.Blog.UnitTests.Web.Features.ShowBlogPost.Components;
+
+public class StructuredDataTests : TestContext
+{
+    [Fact]
+    public void ShouldRenderRawMarkup()
+    {
+        ComponentFactories.AddStub<HeadContent>(ps => ps.Get(p => p.ChildContent));
+
+        var cut = RenderComponent<StructuredData>(
+            ps => ps.Add(p => p.Author, "Steven")
+                .Add(p => p.Headline, "Headline")
+                .Add(p => p.PreviewImage, "url1")
+                .Add(p => p.PreviewFallbackImage, "url2"));
+
+        var element = cut.Find("script");
+        element.Should().NotBeNull();
+    }
+}
