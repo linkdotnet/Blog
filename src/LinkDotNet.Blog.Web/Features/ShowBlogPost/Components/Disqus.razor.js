e393139b9a9af2e5786fa5c687faebf8c8791eb7
Steven Giesel <stgiesel35@gmail.com>
2023-09-20 18:44:49 +0200

    Use const isntead of var in js

--- src/LinkDotNet.Blog.Web/Features/ShowBlogPost/Components/Disqus.razor.js
+++ src/LinkDotNet.Blog.Web/Features/ShowBlogPost/Components/Disqus.razor.js
@@ -1,5 +1,5 @@
 window.initDisqus = (disqus) => {
-    var d = document, s = d.createElement('script');
+    const d = document, s = d.createElement('script');
 
     s.src = `https://${disqus.shortname}.disqus.com/embed.js`;
 
