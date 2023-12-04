6afa75e271d3f1a7166c0f13489acf28cf0ed9a9
Kamyab7 <valipourkamyab.official@gmail.com>
2023-12-04 22:17:30 +0330

    BlogPostId value generator removed

--- src/LinkDotNet.Blog.Infrastructure/Persistence/Sql/Mapping/BlogPostConfiguration.cs
+++ src/LinkDotNet.Blog.Infrastructure/Persistence/Sql/Mapping/BlogPostConfiguration.cs
@@ -1,9 +1,6 @@
-﻿using LinkDotNet.Blog.Domain;
+using LinkDotNet.Blog.Domain;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.EntityFrameworkCore.ValueGeneration;
-using System;
 
 namespace LinkDotNet.Blog.Infrastructure.Persistence.Sql.Mapping;
 
@@ -14,8 +11,7 @@ internal sealed class BlogPostConfiguration : IEntityTypeConfiguration<BlogPost>
         builder.HasKey(c => c.Id);
         builder.Property(c => c.Id)
             .IsUnicode(false)
-            .ValueGeneratedOnAdd()
-            .HasValueGenerator<BlogPostIdGenerator>();
+            .ValueGeneratedOnAdd();
         builder.Property(x => x.Title).HasMaxLength(256).IsRequired();
         builder.Property(x => x.PreviewImageUrl).HasMaxLength(1024).IsRequired();
         builder.Property(x => x.PreviewImageUrlFallback).HasMaxLength(1024);
@@ -28,16 +24,5 @@ internal sealed class BlogPostConfiguration : IEntityTypeConfiguration<BlogPost>
         builder.HasIndex(x => new { x.IsPublished, x.UpdatedDate })
             .HasDatabaseName("IX_BlogPosts_IsPublished_UpdatedDate")
             .IsDescending(false, true);
-    }
-}
-
-
-internal sealed class BlogPostIdGenerator : ValueGenerator<string>
-{
-    public override bool GeneratesTemporaryValues => false;
-
-    public override string Next(EntityEntry entry)
-    {
-        return Guid.NewGuid().ToString("N").Substring(0, 15);
     }
 }
