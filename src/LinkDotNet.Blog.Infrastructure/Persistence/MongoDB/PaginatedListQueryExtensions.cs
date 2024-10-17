fa2b13332bd5c3cc8b395927eb6ffc5dd34d3d65
Steven Giesel <stgiesel35@gmail.com>
2024-10-17 08:06:58 +0200

    Revert "chore: Upgrade packages"
    This reverts commit c8f45dd00adcaf7552958a0354f0c234196b8cc5.

--- src/LinkDotNet.Blog.Infrastructure/Persistence/MongoDB/PaginatedListQueryExtensions.cs
+++ src/LinkDotNet.Blog.Infrastructure/Persistence/MongoDB/PaginatedListQueryExtensions.cs
@@ -1,13 +1,13 @@
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using MongoDB.Driver;
 using MongoDB.Driver.Linq;
 
 namespace LinkDotNet.Blog.Infrastructure.Persistence.MongoDB;
 
 public static class PaginatedListQueryExtensions
 {
-    public static async Task<IPagedList<T>> ToPagedListAsync<T>(this IQueryable<T> source, int pageIndex, int pageSize, CancellationToken token = default)
+    public static async Task<IPagedList<T>> ToPagedListAsync<T>(this IMongoQueryable<T> source, int pageIndex, int pageSize, CancellationToken token = default)
     {
         var count = await source.CountAsync(token);
         if (count > 0)
