50b5525681c4ff43382eda44fde952b3d5e9ba18
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-08-30 13:39:44 +0530

    Add author name in the claim at the time of login, if multi mode is enabled

--- src/LinkDotNet.Blog.Web/Authentication/ILoginManager.cs
+++ src/LinkDotNet.Blog.Web/Authentication/ILoginManager.cs
@@ -1,10 +1,10 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 
 namespace LinkDotNet.Blog.Web.Authentication;
 
 public interface ILoginManager
 {
-    Task SignInAsync(string redirectUri);
+    Task SignInAsync(string redirectUri, string? authorName = null);
 
     Task SignOutAsync(string redirectUri = "/");
 }
--- src/LinkDotNet.Blog.Web/Pages/Login.cshtml.cs
+++ src/LinkDotNet.Blog.Web/Pages/Login.cshtml.cs
@@ -1,20 +1,32 @@
+using System;
 using System.Threading.Tasks;
 using LinkDotNet.Blog.Web.Authentication;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 
 namespace LinkDotNet.Blog.Web.Pages;
 
 public sealed partial class LoginModel : PageModel
 {
     private readonly ILoginManager loginManager;
+    private readonly ApplicationConfiguration applicationConfiguration;
 
-    public LoginModel(ILoginManager loginManager)
+    public LoginModel(ILoginManager loginManager, IOptions<ApplicationConfiguration> options)
     {
+        ArgumentNullException.ThrowIfNull(options);
+
         this.loginManager = loginManager;
+        applicationConfiguration = options.Value;
     }
 
     public async Task OnGet(string redirectUri)
     {
-        await loginManager.SignInAsync(redirectUri);
+        if (!applicationConfiguration.IsMultiModeEnabled)
+        {
+            await loginManager.SignInAsync(redirectUri);
+        }
     }
+
+    public async Task OnPost(string redirectUri, string authorName)
+        => await loginManager.SignInAsync(redirectUri, authorName);
 }
