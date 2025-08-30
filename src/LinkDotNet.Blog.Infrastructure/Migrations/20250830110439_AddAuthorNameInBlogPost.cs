dfe9134abffd2460f21ce2154c2acbd1de5cd83a
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-08-30 16:51:53 +0530

    Add ef migration for author name (address review comments)

---
+++ src/LinkDotNet.Blog.Infrastructure/Migrations/20250830110439_AddAuthorNameInBlogPost.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkDotNet.Blog.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthorNameInBlogPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorName",
+                table: "BlogPosts",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthorName",
+                table: "BlogPosts");
+        }
+    }
+}
