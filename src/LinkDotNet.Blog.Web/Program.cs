1e61610f4987aa4c2e1a8c9325635a6f9c452317
Steven Giesel <stgiesel35@gmail.com>
2025-12-24 10:21:10 +0100

    Revert "refactor: To top level statement"
    This reverts commit ee80c7c20ef82f12c8c51f78f2a6489aad0890b8.

--- src/LinkDotNet.Blog.Web/Program.cs
+++ src/LinkDotNet.Blog.Web/Program.cs
@@ -1,6 +1,6 @@
+using System.Threading.Tasks;
 using Blazored.Toast;
 using HealthChecks.UI.Client;
-using LinkDotNet.Blog.Web;
 using LinkDotNet.Blog.Web.Authentication.OpenIdConnect;
 using LinkDotNet.Blog.Web.Authentication.Dummy;
 using LinkDotNet.Blog.Web.RegistrationExtensions;
@@ -9,87 +9,98 @@ using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
-RegisterServices(builder);
+namespace LinkDotNet.Blog.Web;
 
-await using var app = builder.Build();
-ConfigureApp(app);
-
-await app.RunAsync();
-static void RegisterServices(WebApplicationBuilder builder)
+public class Program
 {
-    builder.Services.AddSecurityHeaderPolicies()
-        .SetDefaultPolicy(p =>
-            p.AddDefaultSecurityHeaders()
-                .AddCrossOriginEmbedderPolicy(policy => policy.UnsafeNone())
-                .AddPermissionsPolicy(policy =>
-                {
-                    policy.AddCamera().None();
-                    policy.AddMicrophone().None();
-                    policy.AddGeolocation().None();
-                }))
-        .AddPolicy("API", p => p.AddDefaultApiSecurityHeaders());
-
-    builder.Services
-        .AddHostingServices()
-        .AddConfiguration()
-        .AddRateLimiting()
-        .AddApplicationServices()
-        .AddStorageProvider(builder.Configuration)
-        .AddImageUploadProvider(builder.Configuration)
-        .AddBlazoredToast()
-        .AddBlazoriseWithBootstrap()
-        .AddResponseCompression()
-        .AddHealthCheckSetup();
-
-    builder.Services.AddAntiforgery();
-
-    if (builder.Environment.IsDevelopment())
+    public static async Task Main(string[] args)
     {
-        builder.Services.UseDummyAuthentication();
-    }
-    else
-    {
-        builder.Services.UseAuthentication();
-    }
-}
+        var builder = WebApplication.CreateBuilder(args);
+        RegisterServices(builder);
 
-static void ConfigureApp(WebApplication app)
-{
-    app.UseSecurityHeaders();
+        await using var app = builder.Build();
+        ConfigureApp(app);
 
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseDeveloperExceptionPage();
-    }
-    else
-    {
-        app.UseExceptionHandler("/Error");
-        app.UseHsts();
+        await app.RunAsync();
     }
 
-    app.UseResponseCompression();
-    app.UseHttpsRedirection();
-    app.MapStaticAssets();
+    private static void RegisterServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddSecurityHeaderPolicies()
+            .SetDefaultPolicy(p =>
+                p.AddDefaultSecurityHeaders()
+                    .AddCrossOriginEmbedderPolicy(policy => policy.UnsafeNone())
+                    .AddPermissionsPolicy(policy =>
+                    {
+                        policy.AddCamera().None();
+                        policy.AddMicrophone().None();
+                        policy.AddGeolocation().None();
+                    }))
+            .AddPolicy("API", p => p.AddDefaultApiSecurityHeaders());
 
-    app.MapHealthChecks("/health",
-            new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse, })
+        builder.Services
+            .AddHostingServices()
+            .AddConfiguration()
+            .AddRateLimiting()
+            .AddApplicationServices()
+            .AddStorageProvider(builder.Configuration)
+            .AddImageUploadProvider(builder.Configuration)
+            .AddBlazoredToast()
+            .AddBlazoriseWithBootstrap()
+            .AddResponseCompression()
+            .AddHealthCheckSetup();
+        
+        builder.Services.AddAntiforgery();
+
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Services.UseDummyAuthentication();
+        }
+        else
+        {
+            builder.Services.UseAuthentication();
+        }
+    }
+
+    private static void ConfigureApp(WebApplication app)
+    {
+        app.UseSecurityHeaders();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseResponseCompression();
+        app.UseHttpsRedirection();
+        app.MapStaticAssets();
+
+        app.MapHealthChecks("/health", new HealthCheckOptions
+        {
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+        })
         .RequireAuthorization();
 
-    app.UseStatusCodePagesWithReExecute("/NotFound");
+        app.UseStatusCodePagesWithReExecute("/NotFound");
+        
+        app.UseRouting();
 
-    app.UseRouting();
+        app.UseUserCulture();
 
-    app.UseUserCulture();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        
+        app.UseAntiforgery();
 
-    app.UseAuthentication();
-    app.UseAuthorization();
-
-    app.UseAntiforgery();
-
-    app.UseRateLimiter();
-    app.MapControllers();
-    app.MapRazorPages();
-    app.MapRazorComponents<App>()
-        .AddInteractiveServerRenderMode();
+        app.UseRateLimiter();
+        app.MapControllers();
+        app.MapRazorPages();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+    }
 }
