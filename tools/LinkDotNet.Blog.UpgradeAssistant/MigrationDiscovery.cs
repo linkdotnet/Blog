65b9a7485ec903a44165fee4cac96049a6ecc19d
Steven Giesel <stgiesel35@gmail.com>
2026-03-11 20:45:27 +0100

    fix: Explicit version

--- tools/LinkDotNet.Blog.UpgradeAssistant/MigrationDiscovery.cs
+++ tools/LinkDotNet.Blog.UpgradeAssistant/MigrationDiscovery.cs
@@ -1,14 +1,17 @@
+using LinkDotNet.Blog.UpgradeAssistant.Migrations;
+
 namespace LinkDotNet.Blog.UpgradeAssistant;
 
 public static class MigrationDiscovery
 {
     public static IReadOnlyList<IMigration> DiscoverAll()
     {
-        return typeof(IMigration).Assembly
-            .GetTypes()
-            .Where(t => t is { IsClass: true, IsAbstract: false }
-                     && typeof(IMigration).IsAssignableFrom(t))
-            .Select(t => (IMigration)Activator.CreateInstance(t)!)
+        return new IMigration[]
+            {
+                new Migration11To12()
+            }
+            .OrderBy(m => Version.TryParse(m.FromVersion, out var v) ? v : new Version(0, 0))
+            .ThenBy(m => Version.TryParse(m.ToVersion, out var v) ? v : new Version(0, 0))
             .ToList();
     }
 }
